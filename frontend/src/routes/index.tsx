58ded2c73cd3a02fb4718177302f2f83ae0e8211
Stefan <floydrise@gmail.com>
2025-04-04 22:04:11 +0300

    Add tailwind

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,13 +1,14 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
-    component: Index,
-})
+export const Route = createFileRoute("/")({
+  component: Index,
+});
 
 function Index() {
-    return (
-        <div className="p-2">
-            <h3>Welcome Home!</h3>
-        </div>
-    )
+  return (
+    <div className="p-2">
+      <h3 className={"text-red-500"}>Welcome Home!</h3>
+      <p className={"text-green-500"}>Heyyy</p>
+    </div>
+  );
 }
