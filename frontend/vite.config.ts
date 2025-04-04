2bf86781161f8712038100e8022b65c3392b61b8
Stefan <floydrise@gmail.com>
2025-04-04 22:18:10 +0300

    Add shadcn and theme context

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
   server: {
     proxy: {
       "/api": {
--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,10 @@
 import { Hono } from "hono";
 import eventsRoute from "./routes/events";
+import { logger } from "hono/logger";
 
 const app = new Hono()
   .basePath("/api")
+  .use(logger())
   .route("/events", eventsRoute)
   .get("/", (c) => c.json({ msg: "Howdy from api 🤠" }));
 
