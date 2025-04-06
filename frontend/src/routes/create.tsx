c037bc5f04f89c23ac52bc28047752d8f81450be
Stefan <floydrise@gmail.com>
2025-04-06 12:24:32 +0300

    Add create event layout

--- frontend/src/routes/create.tsx
+++ frontend/src/routes/create.tsx
@@ -1,9 +1,43 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
+import { Label } from "@/components/ui/label.tsx";
+import { Input } from "@/components/ui/input.tsx";
+import { Calendar } from "@/components/ui/calendar.tsx";
+import { useState } from "react";
+import { Button } from "@/components/ui/button.tsx";
 
-export const Route = createFileRoute('/create')({
+export const Route = createFileRoute("/create")({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/create"!</div>
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  return (
+    <div
+      className={
+        "flex flex-col gap-4 justify-center m-auto mt-10 max-w-[340px]"
+      }
+    >
+      <div>
+        <Label htmlFor="title" className={" text-lg"}>
+          Title
+        </Label>
+        <Input type="text" id="title" placeholder="Event title" />
+      </div>
+      <div>
+        <Label htmlFor="description" className={" text-lg"}>
+          Description
+        </Label>
+        <Input type="text" id="description" placeholder="Short description" />
+      </div>
+      <div className={"flex m-auto"}>
+        <Calendar
+          mode="single"
+          selected={date}
+          onSelect={setDate}
+          className="rounded-md border shadow"
+        />
+      </div>
+      <Button className={"w-fit m-auto"}>Add new event</Button>
+    </div>
+  );
 }
